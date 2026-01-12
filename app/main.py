77784d51d5228d3409a76d29b164789ee62e7bae
Alexey <alexey.zhuravel2004@gmail.com>
2026-01-13 00:01:39 +0100

    Solution (completed...I guess

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ class Distance:
     def __repr__(self) -> str:
         return f"Distance(km={self.km})"
 
-    def __add__(self, other: int) -> "Distance":
+    def __add__(self, other: "Distance | int | float") -> "Distance":
         if isinstance(other, Distance):
             return Distance(
                 self.km + other.km
@@ -29,7 +29,7 @@ class Distance:
             self.km += other
             return self
 
-    def __mul__(self, other: "Distance | int | float") -> "Distance":
+    def __mul__(self, other: int | float) -> "Distance":
         return Distance(self.km * other)
 
     def __truediv__(self, other: int | float) -> "Distance":
